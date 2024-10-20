01b2f13926e0c5d6f7f49d4b97b8dbccb4ca6acb
yusuf <yusuf210409@gmail.com>
2024-10-21 02:17:13 +0700

    added port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 const app = express();
-
-app.listen(8080, "localhost", () => {
-  console.log("Success");
-});
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(port);
 });
